21ea7fd6f12b17b2237cb3784155c85b37a805a4
clinton-van-arnam <113942027+vanarnamc@users.noreply.github.com>
2022-11-10 08:16:58 -0500

    update

--- sketch.js
+++ sketch.js
@@ -1,19 +1,25 @@
 let song, amp;
 
+let volumeAmp= 1800;
+
 function preload() {  
   song = loadSound('audio/BlackStonePond.mp3');
 }
 
 function setup() {
+  pixelDensity(5);
+
   createCanvas(windowWidth, windowHeight);
   amp = new p5.Amplitude();
 
 }
 
 function draw() {
+  background(238,238,238, 10);
   let vol = amp.getLevel();
-  ellipse(height/2, width/2, vol*windowWidth, vol*windowHeight);
-  //console.log(vol)
+  ellipse(height/2, width/2, vol*volumeAmp, vol*volumeAmp);
+  console.log(height)
+
 
 
 }
@@ -28,9 +34,9 @@ function mousePressed() {
   if (song.isPlaying()) {
     // .isPlaying() returns a boolean
     song.stop();
-    background(255, 0, 0);
+    // background(255, 0, 0);
   } else {
     song.loop();
-    background(0, 255, 0);
+    // background(0, 255, 0);
   }
 }
